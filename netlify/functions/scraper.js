008340d6ed874db9231007d6f213de3a25a61c08
Rohit Jain <rj1804567@gmail.com>
2024-01-25 14:51:12 +0530

    na

--- netlify/functions/scraper.js
+++ netlify/functions/scraper.js
@@ -9,14 +9,10 @@ exports.handler = async function(event, context) {
         console.log(contents);
 
         return scrapeIt(url, contents)
-            .then(({ data, response }) => ({
-                statusCode: 200,
-                body: JSON.stringify({ data, status: response.statusCode }, null, 2),
-            }))
-            .catch((error) => ({
-                statusCode: 500,
-                body: JSON.stringify({ error: error.toString() }, null, 2),
-            }));
+            .then(({ data, status }) => {
+                console.log(`Status Code: ${status}`)
+                console.log(data)
+            });
     } catch (e) {
         console.error("Error:", e);
         // Handle the error if there's an issue with the scrapeIt function or others
